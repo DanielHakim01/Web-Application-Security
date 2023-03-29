5f39cc5185fb301bdc031e7bdcd78baf21a0fdcc
DanielHakim01 <107184092+DanielHakim01@users.noreply.github.com>
2023-03-29 13:28:48 +0800

    Update action.php

--- action.php
+++ action.php
@@ -1,44 +1,74 @@
 <?php
 
-// Define the error messages
-$errors = array();
-$error_messages = array(
-    'name' => 'Please enter your name.',
-    'email' => 'Please enter a valid email address.',
-    //'message' => 'Please enter a message.'
-);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // Get the form data
+  $name = $_POST['name'];
+  $matric = $_POST['matricNum'];
+  $curr = $_POST['currAddress'];
+  $home = $_POST['homeAddress'];
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $homePhone = $_POST['homePhone'];
 
-// Sanitize the input
-$name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-//$message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+  // Define regular expressions for validation
+  $nameReg = '/^(?!.*[<>])[a-zA-Z\s]+$/';
+  $matricReg = '/^(?!.*[<>])[a-zA-Z0-9]+$/';
+  $currentReg = '/^(?!.*[<>])[a-zA-Z0-9\s\.,]+$/';
+  $homeReg = '/^(?!.*[<>])[a-zA-Z0-9\s\.,]+$/';
+  $emailReg = '/^(?!.*[<>])[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
+  $phoneReg = '/^(?!.*[<>])\+?\d{8,}$/';
+  $homePhoneReg = '/^(?!.*[<>])\+?\d{8,}$/';
+  
+  
 
-// Validate the input
-if (empty($name)) {
-    $errors['name'] = $error_messages['name'];
+  // Validate the form data using regular expressions
+  $errors = [];
+  if (!preg_match($nameReg, $name))
+  {
+    $errors['name'] = 'Please enter a valid name';
+  }
+
+  if (!preg_match($matricReg, $matric))
+  {
+    $errors['matricNum'] = 'Please enter a valid matric number';
+  }
+
+  if (!preg_match($currentReg, $curr))
+  {
+    $errors['currAddress'] = 'Please enter a valid address';
+  }
+
+  if (!preg_match($homeReg, $home))
+  {
+    $errors['homeAddress'] = 'Please enter a valid home address';
+  }
+
+  if (!preg_match($emailReg, $email))
+  {
+    $errors['email'] = 'Please enter a valid email address';
+  }
+
+  if (!preg_match($phoneReg, $phone))
+  {
+    $errors['phone'] = 'Please enter a valid phone number';
+  }
+  
+  if (!preg_match($homePhoneReg, $homePhone))
+  {
+    $errors['homePhone'] = 'Please enter a valid home number';
+  }
+
+  // Process the form data if there are no validation errors
+  if (empty($errors)) {
+    // TODO: Process the form data (e.g. send an email)
+    $response = ['success' => true];
+  } else {
+    $response = ['success' => false, 'errors' => $errors];
+  }
+
+  // Return the response as JSON
+  header('Content-Type: application/json');
+  echo json_encode($response);
 }
-if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $errors['email'] = $error_messages['email'];
-}
-// if (empty($message)) {
-//     $errors['message'] = $error_messages['message'];
-// }
-
-// If there are errors, return the error messages
-if (!empty($errors)) {
-    header('Content-Type: application/json');
-    echo json_encode(array('errors' => $errors));
-    exit;
-}
-
-// If there are no errors, send the email and return a success message
-$to = 'you@example.com';
-$subject = 'New Message from Your Website';
-$headers = "From: $email\r\nReply-To: $email\r\n";
-//$message_body = "Name: $name\n\nEmail: $email\n\nMessage:\n\n$message";
-//mail($to, $subject, $message_body, $headers);
-header('Content-Type: application/json');
-echo json_encode(array('message' => 'Your message was sent successfully.'));
-exit;
 
 ?>
